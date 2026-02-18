f6b3c34aa9785fb62df1315ff7ea84b50c91b814
Ankan02-Chemistry <ankandas0022@gmail.com>
2026-02-18 15:25:54 +0530

    Chayan added
    Chayan added

--- js/group.js
+++ js/group.js
@@ -104,7 +104,7 @@ const currentMembers = [
 ];
 
 const formerPHDStudents = [
-  { image: "images/members/CHayan Latest.jpeg", name: "Dr. Chayan Kumar Kundu", year: "Ph.D., 2024", linkedIn: "", description: "" },
+  { image: "images/members/Chayan Latest.jpeg", name: "Dr. Chayan Kumar Kundu", year: "Ph.D., 2024", linkedIn: "", description: "" },
   { image: "images/members/kamakshya-prasad-mahata.jpg", name: "Dr. Kamakshya Prasad Mahata", year: "Ph.D., 2024", linkedIn: "", description: "" },
   { image: "images/members/Pratap Kumar Mandal.jpeg", name: "Dr. Pratap Kumar Mandal", year: "Ph.D., 2024", linkedIn: "", description: "Post-Doc position in Prof. Phil Ho Lee group, Kangwon National University, South Korea" },
   { image: "images/members/saikat-chatterjee.png", name: "Dr. Saikat Chatterjee", year: "Ph.D., 2024", linkedIn: "", description: "Post-Doc position in Prof. David C. Powers group, Texas A & M University" },
